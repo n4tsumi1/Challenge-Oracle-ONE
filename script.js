5cda4f431e363644ab81cc68f2a0f06db77ac935
Erika Queiroz <72478111+n4tsumi1@users.noreply.github.com>
2023-09-13 20:23:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function validarDados(form) {
+    event.preventDefault();
+    var error = document.querySelector("#error");
+    error.style.color = "red";
+  
+    var expression = /\w+@\w+\.+[a-z]/;
+  
+    var msgError = [];
+  
+    if (form.nome.value == "") {
+      msgError.push("Preenchimento obrigatório!");
+      form.nome.focus();
+    } else if (form.email.value == "") {
+      msgError.push("Preenchimento obrigatório!");
+      form.email.focus();
+    } else if (form.assunto.value == "") {
+      msgError.push("Preenchimento obrigatório!");
+      form.assunto.focus();
+    } else if (form.mensagem.value == "") {
+      msgError.push("Campo Mensagem é obrigatório.");
+      form.mensagem.focus();
+    } else if (
+      form.email.value.indexOf("@") == -1 ||
+      form.email.value.indexOf(".") == -1
+    ) {
+      msgError.push("Email inválido.");
+    }
+  
+    error.innerHTML = msgError.join(",");
+  }
+  
+  document
+    .querySelector("form")
+    .addEventListener("submit", (evento) => {
+   const formulario = evento.target;
+   validarDados(formulario);
+   formulario.nome.value = "";
+   formulario.email.value = "";
+   formulario.assunto.value = "";
+   formulario.mensagem.value = "";
+    })
+  
